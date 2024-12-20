a0124778ffbd9cddba937e3af58d17ef51af3e19
dyk <59551613+dyk4ever0@users.noreply.github.com>
2024-12-20 10:31:19 +0900

    Update const.js

--- documents/.vuepress/const.js
+++ documents/.vuepress/const.js
@@ -1,3 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+function getMarkdownFiles(dir) {
+  const baseDir = path.resolve(__dirname, '../'); 
+  const targetDir = path.join(baseDir, dir);
+
+  return fs.readdirSync(targetDir)
+    .filter(file => file.endsWith('.md'))
+    .map(file => `${dir}/${file}`);
+}
+
+exports.DevOpsList = getMarkdownFiles('DevOps');
+exports.DataBaseList = getMarkdownFiles('DataBase');
+exports.gitbookList = getMarkdownFiles('gitbook-pages');
+
+/*
 exports.DevOpsList = ["DevOps/docker.md", "DevOps/vupress-github-actions.md"];
 exports.DataBaseList = ["DataBase/Redis.md"];
 exports.gitbookList = ["gitbook-pages/serverUSA.md"];
+*/
